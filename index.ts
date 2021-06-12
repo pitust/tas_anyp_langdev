ff1ddcbbfafb14a3bf54957a340e6e02f915bcc7
pitust <piotr@stelmaszek.com>
2021-06-12 20:00:50 +0100

    Auto commit: Sat Jun 12 20:00:50 IST 2021
    Signed-off-by: pitust <piotr@stelmaszek.com>

--- index.ts
+++ index.ts
@@ -112,7 +112,7 @@ function gettoken(tok: TokenType, s: string | null = null): string {
     return ts.shift()[1]
 }
 function istoken(tok: TokenType, s: string | null = null): boolean {
-    if (ts.length == 0) return
+    if (ts.length == 0) return false
     if (ts[0][0] != tok) return false
     if (ts[0][1] != s && s !== null) return false
     return true
