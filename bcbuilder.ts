0ab30ce8241b16335a293bf72c92b496c8156388
pitust <piotr@stelmaszek.com>
2021-06-12 21:13:01 +0100

    Auto commit: Sat Jun 12 21:13:01 IST 2021
    Signed-off-by: pitust <piotr@stelmaszek.com>

--- bcbuilder.ts
+++ bcbuilder.ts
@@ -177,7 +177,7 @@ function writeOp(o: Node, tgd: symbol = null) {
         out.push(new LoadOpcode(tgd, o.params.sym))
         return
     }
-    if (o.name == 'Call')
+    if (o.name == 'CallExpr')
     if (o.name == 'Noop') {
         return
     }
