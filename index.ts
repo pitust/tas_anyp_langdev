de9736c0fba50f0cc5fd1d914cd4ce18d9c48070
pitust <piotr@stelmaszek.com>
2021-06-12 18:45:32 +0100

    Auto commit: Sat Jun 12 18:45:32 IST 2021
    Signed-off-by: pitust <piotr@stelmaszek.com>

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
-// let world = 'world'
-// let a = :ok;
-// print("Hello, {world}")
-// inspect(match a {
-//     :ok => 3
-//     :notok => 4
-// })
+let world = 'world'
+let a = :ok;
+print("Hello, {world}")
+inspect(match a {
+    :ok => 3
+    :notok => 4
+})
 
-// program 
 
 
