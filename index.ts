b1756797378da5370a001e6251284495013e5297
pitust <piotr@stelmaszek.com>
2021-06-12 21:12:34 +0100

    Auto commit: Sat Jun 12 21:12:34 IST 2021
    Signed-off-by: pitust <piotr@stelmaszek.com>

--- index.ts
+++ index.ts
@@ -214,7 +214,7 @@ expr = (() => {
                 if (!istoken(TokenType.Punct, ')')) gettoken(TokenType.Punct, ',')
             }
             gettoken(TokenType.Punct, ')')
-            return Node('CallExpr', { args: Node('Args', Object.fromEntries(args.map((e => )), {}) }, {  })
+            return Node('CallExpr', { args: Node('Args', Object.fromEntries(args.map((e, i) => [i, e])), {}), calle }, {  })
         }
         return a
     }
