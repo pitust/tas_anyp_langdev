27e5c68c14ed982033565251f35153cdb65d0d74
pitust <piotr@stelmaszek.com>
2021-06-12 18:54:52 +0100

    Auto commit: Sat Jun 12 18:54:52 IST 2021
    Signed-off-by: pitust <piotr@stelmaszek.com>

---
+++ index.ts
@@ -0,0 +1,24 @@
+sc = ";";
+
+(* compiler magic *)
+stratom = __;
+
+blockatom = "{" ([^"}"]stmt)* "}";
+ifatom = kw:"if" expr blockatom ["else"]("else" blockatom);
+
+pattern = symbolatom | ident;
+match_body = [","](",") pattern "=>" expr;
+
+matchatom = kw:"match" "{" match_body "}";
+symbolatom = ":" ident;
+identatom = ident;
+
+atom = stratom | symbolatom | identatom | ifatom | blockatom | matchatom;
+
+(* prec climber *)
+expr = prec_climb[* / - +] atom;
+
+letstmt = kw:"let" ident:* "=" expr;
+exprstmt = expr;
+stmt = sc | letstmt | exprstmt;
+program = [^eof](stmt program);
