9ce5866794be4537587fd23522e695662ec713e4
pitust <piotr@stelmaszek.com>
2021-06-12 21:11:26 +0100

    Auto commit: Sat Jun 12 21:11:26 IST 2021
    Signed-off-by: pitust <piotr@stelmaszek.com>

--- index.ts
+++ index.ts
@@ -214,7 +214,7 @@ expr = (() => {
                 if (!istoken(TokenType.Punct, ')')) gettoken(TokenType.Punct, ',')
             }
             gettoken(TokenType.Punct, ')')
-            return Node
+            return Node('CallExpr', { ...[] } )
         }
         return a
     }
